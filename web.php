8a738ca192ce3c07c63bcd82940da9b83c731a82
kazu <kazumaprun@gmail.com>
2023-07-09 22:39:46 +0900

    web.php 2023.07.09

--- web.php
+++ web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function (){
+/*Route::get('/', function (){
 
     return view('welcome');
     
 });
+*/
 
-Route::get('hello','HelloController@index');
-Route::get('hello/other','HelloController@other');
+
+Route::get('hello',function() {
+    return view('hello.index');
+});
