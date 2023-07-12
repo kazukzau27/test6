a51bcc45c890bfca285644b00c532f4196d456c0
kazu <kazumaprun@gmail.com>
2023-07-12 21:48:18 +0900

    web.php p63

--- web.php
+++ web.php
@@ -14,14 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-/*Route::get('/', function (){
+Route::get('/', function (){
 
     return view('welcome');
     
 });
-*/
 
 
-Route::get('hello',function() {
-    return view('hello.index');
-});
+
+Route::get('hello', 'HelloController@index');
+
