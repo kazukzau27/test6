13017f6f76d9b2f438a35e3a1201f556480c706c
kazu <kazumaprun@gmail.com>
2023-07-12 21:56:28 +0900

    index.php p63

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
     h1 {font-size:100pt; text-align:right; color:#f6f6f6;
         margin:-50px 0px -100px 0px;}
     </style>
-</style>
+</head>
 <body>
     <h1>Index</h1>
-    <p>This is a sample page with php-template.</p>
+    <p><?php echo $msg; ?></p>
+    <p><?php echo date("Y年n月j日"); ?></p>
 </body>
 </html>
 
