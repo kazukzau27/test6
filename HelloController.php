24c5c43e47829114324c072234579ff64f6d0906
kazu <kazumaprun@gmail.com>
2023-07-16 21:38:09 +0900

    HelloController.php p72

--- HelloController.php
+++ HelloController.php
@@ -9,11 +9,20 @@ class HelloController extends Controller
 {
     public function index() 
     {
-        $data = ['msg'=>'これはコントローラーから渡されたメッセージです。'];
+        $data = [
+            'msg'=>'お名前を入力してください。'];
 
         return view('hello.index', $data);
     
 
+    }
+
+    public function post(Request $request)
+    {
+        $msg = $request->msg;
+
+        $data = ['msg'=>'こんにちは、'. $msg . 'さん!'];
+        return view('hello.index',$data);
     }
 
     
