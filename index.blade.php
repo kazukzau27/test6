e926414c60735f8b84f77212857707b0bbd960a0
kazu <kazumaprun@gmail.com>
2023-08-01 21:12:34 +0900

    index.blade.php p64

--- index.blade.php
+++ index.blade.php
@@ -1,26 +1,21 @@
-<html>
-    <head>
-        <title>Hello</title>
-        <style>
-            body {font-size:16pt; color:#999;}
-            h1 {font-size:50pt; text-align:right; color:#f6f6f6;
-                margin:-20px 0px -30px 0px; letter-spacing:-4pt;}
-        </style>
-    </head>
-    <body>
-        <h1>Blade/Index</h1>
-        <p>&#064;foreachディレクティブの例</p>
-        <ol>
-        @for ($i = 1;$i < 100;$i++)
-        @if ($i % 2 == 1)
-                @continue
-        @elseif ($i <= 10)
-        <li>No, {{$i}}
-        @else
-                @break
-        @endif
-        @endfor
-        </ol>
+@extends('layouts.helloapp')
+@section('title','Index')
 
-    </body>
-</html>
+@section('menubar')
+    @parent
+    インデックスページ
+    
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <ul>
+        @each('components.item',$data,'item')
+    </ul>
+
+    
+@endsection
+
+@section('footer')
+copyright 2023 hirata.
+@endsection
